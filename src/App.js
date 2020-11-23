6de2200c4def7ddc1d6ef37d39f4030e0184bcf0
kardrean <kardrean@ecodev.com>
2020-11-23 16:25:50 -0300

    version 1.1

--- src/App.js
+++ src/App.js
@@ -59,8 +59,7 @@ export default () =>{
         }
       </section>
       <footer>
-        Feito em Live (https://www.youtube.com/watch?v=tBweoUiMsDg) para estudo de react, todos os direitos das imagens são da Netflix.
-        Dados Extraidos de https://www.themoviedb.org/
+        Feito por Kardrean agradecimentos profº: Bonieky Lacerda
       </footer>
 
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ export default ({black})=>{
     return(
         <header className={black ?"black" : ''}>
             <div className="header--logo">
-                <img src="./logo.png"/>
+                <img src="https://github.com/kardrean/clone_NETFLIX_React/blob/main/src/components/Header/logo.png?raw=true"/>
             </div>
             <div className="header--user">
                 <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"/>
